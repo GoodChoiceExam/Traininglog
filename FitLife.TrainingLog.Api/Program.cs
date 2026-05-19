f9d81e339ce4f2205878178063811fc0e772d2e0
EberFras™ <elias@liebman.dk>
2026-05-19 20:39:58 +0200

    chore: ret rækkefølge i Program.cs og standardiser TokenValidationParameters

--- FitLife.TrainingLog.Api/Program.cs
+++ FitLife.TrainingLog.Api/Program.cs
@@ -1,11 +1,11 @@
 using System.Text;
-using Microsoft.OpenApi;
 using FitLife.TrainingLog.Api.Repositories;
 using FitLife.TrainingLog.Api.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-using MongoDB.Bson.Serialization.Serializers;
+using Microsoft.OpenApi;
 using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 using NLog;
 using NLog.Web;
@@ -21,21 +21,15 @@ try
     builder.Logging.ClearProviders();
     builder.Host.UseNLog();
 
-    var mongoClient = new MongoClient(builder.Configuration["MongoDB:ConnectionString"]);
-    var database = mongoClient.GetDatabase(builder.Configuration["MongoDB:DatabaseName"]);
-    builder.Services.AddSingleton(database);
-
-    builder.Services.AddSingleton<IWorkoutRepository, WorkoutRepository>();
-    builder.Services.AddSingleton<IWorkoutService, WorkoutService>();
-
     var jwtIssuer = builder.Configuration["Jwt:Issuer"] ?? "fitlife-identity";
     var jwtAudience = builder.Configuration["Jwt:Audience"] ?? "fitlife";
     var secret = builder.Configuration["Jwt:Secret"] ?? "dev-secret-change-in-production";
 
-    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         .AddJwtBearer(options =>
         {
-            options.TokenValidationParameters = new()
+            options.TokenValidationParameters = new TokenValidationParameters
             {
                 ValidateIssuer = true,
                 ValidIssuer = jwtIssuer,
@@ -62,6 +56,7 @@ try
         });
 
     builder.Services.AddAuthorization();
+
     builder.Services.AddCors(options =>
     {
         options.AddPolicy("Frontend", policy =>
@@ -69,6 +64,13 @@ try
                 .AllowAnyHeader()
                 .AllowAnyMethod());
     });
+
+    var mongoClient = new MongoClient(builder.Configuration["MongoDB:ConnectionString"]);
+    var database = mongoClient.GetDatabase(builder.Configuration["MongoDB:DatabaseName"]);
+    builder.Services.AddSingleton(database);
+
+    builder.Services.AddSingleton<IWorkoutRepository, WorkoutRepository>();
+    builder.Services.AddSingleton<IWorkoutService, WorkoutService>();
     builder.Services.AddControllers()
         .AddJsonOptions(options =>
             options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter()));
